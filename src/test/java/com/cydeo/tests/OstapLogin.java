b424ecafca21748e3d40296478d8e638d0bc7e1e
Ostap <ostapoz1997@gmail.com>
2022-09-16 12:51:41 -0400

    OstapLogin.java class created

---
+++ src/test/java/com/cydeo/tests/OstapLogin.java
@@ -0,0 +1,14 @@
+package com.cydeo.tests;
+
+public class OstapLogin {
+    public static void main(String[] args) {
+
+
+        // Ostap class is created
+
+        // updated
+
+        // completed
+
+    }
+}
